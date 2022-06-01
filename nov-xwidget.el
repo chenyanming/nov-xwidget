77657ca7ba66b57cc90f05d63d155a3b6a9ecb6c
chenyanming <elecming@gmail.com>
2022-06-01 19:50:26 +0800

    First commit

---
+++ nov-xwidget.el
@@ -0,0 +1,398 @@
+;;; nov-xwidget.el --- nov-xwidget - the best epub reader in Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Damon Chan
+
+;; Author: Damon Chan <elecming@gmail.com>
+;; URL: https://github.com/chenyanming/nov-xwidget
+;; Keywords: hypermedia, multimedia, epub
+;; Created: 1 June 2022
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1") (nov "0.4.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; nov-xwidget - the best epub reader in Emacs
+
+;;; Code:
+
+(require 'nov)
+(require 'shr)
+(require 'xwidget)
+
+(defcustom nov-xwdiget-script (format "
+// Your CSS as text
+var title = `
+   hello
+`
+var meta = document.createElement(\"meta\")
+// meta.setAttribute('http-equiv', 'Content-Type' )
+// meta.setAttribute('content', 'text/html' )
+meta.setAttribute('charset', 'utf-8' )
+document.getElementsByTagName('head')[0].appendChild(meta);
+
+" "")
+  "Javascript scripts used to run in the epub file."
+  :group 'nov-xwidget
+  :type 'string)
+
+
+(defcustom nov-xwdiget-style-light (format "
+    body {
+        writing-mode: horizontal-tb;
+        // background: %s !important;
+        font-size: 18px !important;
+        max-width: 750px !important;
+        text-align: left !important;
+        width: 90%% !important;
+        height: 50%% !important;
+        position: absolute !important;
+        left: 49%% !important;
+        top: 30%% !important;
+        transform: translate(-50%%, -55%%) !important;
+    }
+    p {
+        font-size: 1em !important;
+        text-align: left !important;
+        line-height: 1.3 !important;
+        margin-bottom: 25px !important;
+    }
+    pre, tr, td, div.warning {
+        font-size: 1em;
+        background: #d8dee9;
+    }
+    th {
+        font-size: 1em;
+    }
+    span {
+        font-size: 18px;
+    }
+    /* Same font for all tags */
+    a, em, caption, th, pre, tr, td, code, h1, h2, h3, h4, h5, h6, p, body {
+        font-family: \"Fira Code\", Georgia,Cambria,\"Times New Roman\",Times,serif !important;
+    }
+    h1 {
+        font-size: 2em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    h2 {
+        font-size: 1.5em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    h3 {
+        font-size: 1.3em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    h4 {
+        font-size: 1.2em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    h5 {
+        font-size: 1.1em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    h6 {
+        font-size: 1em !important;
+        color: #2e3440 !important;
+        margin-bottom: 10px !important;
+    }
+    code {
+        font-size: 1em !important;
+    }
+    :root {
+        color-scheme: light; /* both supported */
+    }
+
+    body img {
+        max-width: 100%% !important;
+    }
+    .programlisting {
+        font-size: 20px;
+    }
+" (face-attribute 'default :background))
+  "Light mode CSS style used to render the epub file."
+  :group 'nov-xwidget
+  :type 'string)
+
+
+(defcustom nov-xwdiget-style-dark (format "
+    body {
+        writing-mode: horizontal-tb;
+        // background: %s !important;
+        color: #eee !important;
+        font-size: 18px !important;
+        max-width: 750px !important;
+        text-align: left !important;
+        width: 90%% !important;
+        height: 50%% !important;
+        position: absolute !important;
+        left: 49%% !important;
+        top: 30%% !important;
+        transform: translate(-50%%, -55%%) !important;
+    }
+    p {
+        text-align: left !important;
+        line-height: 1.3 !important;
+        margin-bottom: 25px !important;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        /*color: #eee !important;*/
+        border-bottom: 0px solid #eee !important;
+    }
+    pre, tr, td, div.warning {
+        font-size: 1em;
+        background: #272c35;
+    }
+    th {
+        font-size: 1em;
+        color: #eee !important;
+    }
+
+    span {
+        font-size: 18px;
+        color: #eee !important;
+    }
+    h1 {
+        color: #ffaf69 !important;
+    }
+    h2 {
+        color: #3fc6b7 !important;
+    }
+    h3 {
+        color: #88d498 !important;
+    }
+    h4 {
+        color: #80c3f0 !important;
+    }
+    h5 {
+        color: #cccccc !important;
+    }
+    h6 {
+        color: #cccccc !important;
+    }
+
+    /* Same font for all tags */
+    a, em, caption, th, pre, tr, td, code, h1, h2, h3, h4, h5, h6, p, body {
+        font-family: \"Fira Code\", Georgia,Cambria,\"Times New Roman\",Times,serif !important;
+    }
+    code {
+        font-size: 1em !important;
+    }
+    :root {
+        color-scheme: dark; /* both supported */
+    }
+
+    body, p.title  {
+        color: #eee !important;
+    }
+
+    body a{
+        color: #809fff !important;
+    }
+
+    body img {
+        max-width: 100%% !important;
+        filter: brightness(.8) contrast(1.2);
+    }
+    .programlisting {
+        font-size: 20px;
+    }
+" (face-attribute 'default :background))
+  "Dark mode CSS style used to render the epub file."
+  :group 'nov-xwidget
+  :type 'string)
+
+(defun nov-xwidget-webkit-find-file (candidate &optional arg new-session)
+  "Open file with webkit."
+  (interactive
+   (list
+    (pcase major-mode
+      ('nov-mode
+       (cdr (aref nov-documents nov-documents-index)))
+      (_
+       (read-file-name "Webkit find file: ")))
+    current-prefix-arg))
+  (let* ((native-path (expand-file-name (if (eq major-mode 'calibredb-search-mode)
+                                            (calibredb-get-file-path candidate t)
+                                          candidate)))
+         ;; only work on html/xhtml file, rename xhtml as html
+         (output-native-file-name (if (or (string-equal (file-name-extension native-path) "html")
+                                          (string-equal (file-name-extension native-path) "xhtml"))
+                                      (format "_%s.html" (file-name-base native-path))
+                                    (file-name-nondirectory native-path)))
+         ;; get full path of the final html file
+         (output-native-path (expand-file-name output-native-file-name (file-name-directory native-path)))
+         ;; create the html if not esists, insert the `nov-xwdiget-script' as the html script
+         (dom (with-temp-buffer
+                 (insert-file-contents native-path)
+                 (libxml-parse-html-region (point-min) (point-max))))
+         (title (format "%s: %s" (alist-get 'title nov-metadata) (dom-text (dom-by-tag dom 'title))))
+         (new-dom (let ((dom dom))
+                    (dom-append-child
+                     (dom-by-tag dom 'head)
+                     '(meta ((charset . "utf-8"))))
+                    (dom-append-child
+                     (dom-by-tag dom 'head)
+                     `(style nil ,(pcase (frame-parameter nil 'background-mode)
+                                    ('light nov-xwdiget-style-light)
+                                    ('dark nov-xwdiget-style-dark)
+                                    (_ nov-xwdiget-style-light))))
+                    (setf (elt (car (dom-by-tag dom 'title)) 2) title)
+                    dom))
+         (file (with-temp-file output-native-path
+                 (shr-dom-print new-dom)
+                 ;; (encode-coding-region (point-min) (point-max) 'utf-8)
+                 output-native-path))
+         ;; get web url of the file
+         (path (replace-regexp-in-string
+               " "
+               "%20"
+               (concat
+                "file:///"
+                file))))
+    (if arg
+        (let ((calibredb-preferred-format nil))
+          (nov-xwidget-webkit-browse-url-other-window path new-session 'switch-to-buffer))
+      (nov-xwidget-webkit-browse-url-other-window path new-session 'switch-to-buffer))))
+
+(defun nov-xwidget-webkit-browse-url-other-window (url &optional new-session switch-buffer-fun)
+  "Ask xwidget-webkit to browse URL.
+NEW-SESSION specifies whether to create a new xwidget-webkit session.
+Interactively, URL defaults to the string looking like a url around point."
+  (interactive (progn
+                 (require 'browse-url)
+                 (browse-url-interactive-arg "xwidget-webkit URL: "
+                                             ;;(xwidget-webkit-current-url)
+                                             )))
+  (or (featurep 'xwidget-internal)
+      (user-error "Your Emacs was not compiled with xwidgets support"))
+  (require 'xwidget)
+  (when (stringp url)
+    (if new-session
+        (xwidget-webkit-new-session url)
+      (progn
+        (xwidget-webkit-goto-url url)
+        (if switch-buffer-fun
+            (funcall switch-buffer-fun (xwidget-buffer (xwidget-webkit-current-session)))
+          (pop-to-buffer (xwidget-buffer (xwidget-webkit-current-session))))))))
+
+(defun nov-xwidget-view ()
+  "View the current document in a xwidget webkit buffer."
+  (interactive)
+  (let* ((docs nov-documents)
+         (index nov-documents-index)
+         (toc nov-toc-id)
+         (epub nov-epub-version)
+         (metadata nov-metadata))
+
+    ;; open the html file
+    (nov-xwidget-webkit-find-file (cdr (aref docs index)) nil t)
+    ;; save nov related local variables
+    (with-current-buffer (xwidget-buffer (xwidget-webkit-current-session))
+      (setq-local nov-documents docs)
+      (setq-local nov-documents-index index)
+      (setq-local nov-toc-id toc)
+      (setq-local nov-epub-version epub)
+      (setq-local nov-metadata metadata)
+      ;(setq-local imenu-create-index-function 'my-nov-imenu-create-index)
+      )))
+
+(defun nov-xwidget-next-document ()
+  "Go to the next document and render it."
+  (interactive)
+  (when (< nov-documents-index (1- (length nov-documents)))
+    (let* ((docs nov-documents)
+           (index (1+ nov-documents-index))
+           (toc nov-toc-id)
+           (epub nov-epub-version)
+           (metadata nov-metadata)
+           (path (cdr (aref docs index))))
+      (nov-xwidget-webkit-find-file path)
+      (with-current-buffer (buffer-name)
+        (setq-local nov-documents docs)
+        (setq-local nov-documents-index index)
+        (setq-local nov-toc-id toc)
+        (setq-local nov-metadata metadata)
+        (setq-local nov-epub-version epub)))))
+
+(defun nov-xwidget-previous-document ()
+  "Go to the previous document and render it."
+  (interactive)
+  (when (> nov-documents-index 0)
+    (let* ((docs nov-documents)
+           (index (1- nov-documents-index))
+           (toc nov-toc-id)
+           (epub nov-epub-version)
+           (metadata nov-metadata)
+           (path (cdr (aref docs index))))
+      (if (string-equal (file-name-extension path) "ncx")
+          (nov-xwidget-goto-toc)
+        (nov-xwidget-webkit-find-file path)
+        (with-current-buffer (buffer-name)
+          (setq-local nov-documents docs)
+          (setq-local nov-documents-index index)
+          (setq-local nov-toc-id toc)
+          (setq-local nov-metadata metadata)
+          (setq-local nov-epub-version epub))))))
+
+(defun nov-xwidget-goto-toc ()
+  "Go to the TOC index and render the TOC document."
+  (interactive)
+  (let* ((docs nov-documents)
+         (epub nov-epub-version)
+         (ncxp (version< nov-epub-version "3.0"))
+         (index (nov-find-document (lambda (doc) (eq (car doc) nov-toc-id))))
+         (toc nov-toc-id)
+         (path (cdr (aref docs index)))
+         (html-path (expand-file-name "toc.html" (file-name-directory path)))
+         (html (if (file-exists-p html-path)
+                   (with-temp-buffer (insert-file-contents html-path) (buffer-string))
+                 (nov-ncx-to-html path)))
+         (dom (with-temp-buffer
+                (if ncxp
+                    (insert html)
+                  (insert-file-contents path))
+                (libxml-parse-html-region (point-min) (point-max))))
+         (new-dom (let ((dom dom))
+                    (dom-add-child-before
+                     dom
+                     `(head nil
+                            (meta ((charset . "utf-8")))
+                            (title nil "TOC")
+                            (style nil ,(pcase (frame-parameter nil 'background-mode)
+                                          ('light nov-xwdiget-style-light)
+                                          ('dark nov-xwdiget-style-dark)
+                                          (_ nov-xwdiget-style-light)))
+                            (script nil ,nov-xwdiget-script)))
+                    dom))
+         (file (with-temp-file html-path
+                 (shr-dom-print new-dom)
+                 html-path)))
+    (when (not index)
+      (error "Couldn't locate TOC"))
+    (nov-xwidget-webkit-find-file file)
+    (with-current-buffer (buffer-name)
+      (setq-local nov-documents docs)
+      (setq-local nov-documents-index index)
+      (setq-local nov-toc-id toc)
+      (setq-local nov-epub-version epub))))
+
+(provide 'nov-xwidget)
+;;; nov-xwidget ends here
